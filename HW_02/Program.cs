f533ef1dcca659f95dec6e7e45866f08078043e0
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-09-24 20:56:01 +0300

    lesson 2 project

--- HW_02/Program.cs
+++ HW_02/Program.cs
@@ -3,10 +3,17 @@
 namespace HW_02
 {
     class Program
-    {
-        static void Main(string[] args)
+    {   static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+            byte num1 = 23;
+            short num2 = num1;
+
+            byte num3 = (byte)num2;
+
+            object obj;
+            System.Object obj1;
+
+       }
     }
 }
