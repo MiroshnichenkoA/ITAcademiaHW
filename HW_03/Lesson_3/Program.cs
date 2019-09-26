4bd67c652dff06d3c64b7c6470726c46d9a893f2
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-09-26 19:59:23 +0300

    lesson 3 working tast

---
+++ HW_03/Lesson_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int num1 = 10;
+            num1 = num1 + 1;
+            num1 += 1;
+
+            int num2 = 30;
+            bool result = (num1 == num2);
+
+            string num3 = "Hi!";
+
+            bool isString = num3 is string;
+
+
+
+
+        }
+    }
+}
